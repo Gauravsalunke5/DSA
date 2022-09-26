8fd304f69795fb252b9e9f7775bc71b1040371b9
Gaurav Salunke <gauravsalunke15@gmail.com>
2022-09-26 20:40:26 +0530

    26_09_2022

---
+++ DSACode/src/com/dsa/array/MatrixRotation90.java
@@ -0,0 +1,78 @@
+package com.dsa.array;
+
+public class MatrixRotation90 {
+
+	static int N = 4;
+
+	// In-place rotate it by 90 degrees in clockwise direction
+	static void matrixRotation(int matrix[][]) {
+
+		for (int i = 0; i < N / 2; i++) {
+			for (int j = i; j < N - i - 1; j++) {
+				int temp = matrix[i][j];
+				matrix[i][j] = matrix[N - 1 - j][i];
+				matrix[N - 1 - j][i] = matrix[N - i - 1][N - 1 - j];
+				matrix[N - 1 - i][N - 1 - j] = matrix[j][N - 1 - i];
+				matrix[j][N - 1 - i] = temp;
+
+			}
+		}
+	}
+
+	static void printMatrix(int matrix[][]) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++)
+				System.out.print(matrix[i][j] + " ");
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) { 
+        int matrix[][] = { { 1, 2, 3, 4 }, 
+                { 5, 6, 7, 8 }, 
+                { 9, 10, 11, 12 }, 
+                { 13, 14, 15, 16 } }; 
+        /*13 9 5 1 
+		14 10 6 2 
+		15 11 7 3 
+		16 12 8 4 */
+matrixRotation(matrix); 
+printMatrix(matrix); 
+}
+}
+
+	/* Try more Inputs
+	case 1: 
+	int matrix[][] = { { 1, 2, 3}, 
+	                   { 4, 5, 6}, 
+	                   { 7, 8, 9}}; 
+
+	actual = matrixRotation(matrix)
+	expected = 7  4  1 
+	           8  5  2 
+	           9  6  3
+	*/
+	/*
+	
+	In-place techniques matrix rotation method by 90 degree
+	Asked In: Facebook Google Amazon Microsoft
+	You are given a square matrix, You need to rotate the matrix in a clockwise direction by 90 degrees in Time Complexity O(m*n) and No Extra Space i.e O(1) Asked in : Facebook, Google, Amazon, Microsoft
+
+	Example:
+
+	Input : 
+	If the array is
+	[
+	    [1, 2],
+	    [3, 4]
+	]
+	Output:
+	Rotated array becomes:
+	[
+	    [3, 1],
+	    [4, 2]
+	]
+	Hint1 : Select 4 numbers from each corner of matrix and rotate clock wise 
+	Hint2 : Repeat this process for every layer of matrix
+	Problem level: Hard
+	*/
---
+++ DSACode/src/com/dsa/array/SearchElementInSortedMatrix.java
@@ -0,0 +1,71 @@
+package com.dsa.array;
+
+import java.util.Iterator;
+
+public class SearchElementInSortedMatrix {
+
+	private static void searchElement(int[][] matrix, int x, int value) 
+	{
+		int r = matrix.length;
+		int c = matrix[0].length;
+		int i = 0, j = c - 1;
+		while (i < r && j >= 0) {
+			if (matrix[i][j] == value) {
+				System.out.println("Found " + i + " " + j);
+				return;
+			}
+			if (matrix[i][j] > value)
+				j--;
+			else
+				i++;
+		}
+		System.out.println("Not Found");
+	} 
+	
+	public static void main(String[] args) 
+	{ 
+		int matrix[][] = { { 10, 20, 30, 40 }, 
+						{ 15, 25, 36, 46 }, 
+						{ 28, 29, 37, 48 }, 
+						{ 32, 33, 39, 50 } }; 
+	//	int matrix[][] = { { 1},{3}};
+		 
+		searchElement(matrix, 2, 32); 
+	} 
+} 
+
+/* Try more Inputs
+
+case1:
+ actual = searchElement([[10, 20, 30, 40], 
+		[15, 25, 36, 46], 
+		[27, 29, 37, 48],
+		[32, 33, 39, 50]],4,32)
+ expected = True
+ 
+case2:
+actual = searchElement([[10, 20, 30, 40],
+		[15, 25, 36, 46], 
+		[27, 29, 37, 48], 
+		[32, 33, 39, 50]],4,59)
+expected = False
+
+*/
+
+/*
+ * Search Element in Matrix
+Asked In: Cisco Vmware Amazon
+Matrix n*n is given, where all elements in any individual row or column are sorted. In such a matrix, we have to find the position of a value in O(n) Time Complexity and O(1) Space Complexity
+
+Example:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 3
+Output: true
+Problem level: Medium
+*/
